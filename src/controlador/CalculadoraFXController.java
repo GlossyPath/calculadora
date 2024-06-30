d23ffa1ea689fc95a6ec751d3cccd99979bbd42f
GlossyPath <glossypath0808@gmail.com>
2024-06-30 22:20:56 +0200

    Cuadricula básica de calculadora creada + modelo

--- src/controlador/CalculadoraFXController.java
+++ src/controlador/CalculadoraFXController.java
@@ -1,5 +1,11 @@
 package controlador;
 
+import javafx.fxml.FXML;
+
+import javafx.scene.control.TextField;
+
 public class CalculadoraFXController {
-	
+	@FXML
+	private TextField tfContador;
+
 }
---
+++ src/modelo/Contador.java
@@ -0,0 +1,21 @@
+package modelo;
+
+public class Contador {
+
+	private int valor;
+	
+	public Contador() {
+		
+		this.valor = 0;
+	}
+
+	public int getValor() {
+		return valor;
+	}
+
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+	
+	
+}
