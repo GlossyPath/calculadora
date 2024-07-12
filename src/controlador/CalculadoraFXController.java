bc2fe52037b48d7340e180d056b8a9b463506c76
GlossyPath <glossypath0808@gmail.com>
2024-07-13 00:27:44 +0200

    lógica mejorada, falta capturar excepciones

--- src/controlador/CalculadoraFXController.java
+++ src/controlador/CalculadoraFXController.java
@@ -19,6 +19,7 @@ public class CalculadoraFXController implements Initializable {
             btMultiplicacion, btSiete, btOcho, btnueve, btDivision;
 
     private Contador contador;
+    
     private String numero;
     private String operador;
     private String acumulado;
@@ -31,92 +32,118 @@ public class CalculadoraFXController implements Initializable {
         this.acumulado = "";
     }
 
+    
     public void añadirSigno (String btnTexto) {
-    	
     	operador = btnTexto;
         tfContador.setText(btnTexto);
         contador.setValor(Integer.parseInt(this.acumulado));
-        this.acumulado = "";
-        this.numero = "";
-        
+        this.acumulado = "";     
     }
+    
+    public void añadirPrimerDigito(String btnTexto) {
+    	this.numero = btnTexto;
+        tfContador.setText(this.numero);
+        this.acumulado = this.numero;       
+        contador.setValor(Integer.parseInt(this.acumulado));
+    }
+    
+    public void añadirDigitos(String btnTexto) {
+    	 if (this.acumulado.equals("")) {
+             tfContador.setText(btnTexto);
+    	 }
+    	 
+    	 this.numero = btnTexto;
+         this.acumulado += this.numero;
+         tfContador.setText(this.acumulado);
+    }
+    
     @FXML
     public void clickBoton(ActionEvent event) {
         Button btn = (Button) event.getSource();
         String btnTexto = btn.getText();
-
-        if (operador.isEmpty()) {
-        	
-            if ((!btnTexto.equals("+") && !btnTexto.equals("-") && !btnTexto.equals("*") && !btnTexto.equals("/")) && this.numero.isEmpty()) {
+        
+        if (operador.equals("")) {
             	
-                this.numero = btnTexto;
-                tfContador.setText(this.numero);
-                this.acumulado = this.numero;       
-                contador.setValor(Integer.parseInt(this.acumulado));
+        	if ((!btnTexto.equals("+") && !btnTexto.equals("-") && !btnTexto.equals("*") && !btnTexto.equals("/")) && this.numero.isEmpty()) {
+                	
+                añadirPrimerDigito(btnTexto);
+                    
+        	} else if ((!btnTexto.equals("+") && !btnTexto.equals("-") && !btnTexto.equals("*") && !btnTexto.equals("/")) && !this.acumulado.isEmpty()) {
+                	
+                añadirDigitos(btnTexto);
+                    
+        	} else if (btnTexto.equals("+") || btnTexto.equals("-") || btnTexto.equals("*") || btnTexto.equals("/") && btnTexto.isEmpty()) {
+                	
+                añadirSigno(btnTexto);
+        	}
                 
-            } else if ((!btnTexto.equals("+") && !btnTexto.equals("-") && !btnTexto.equals("*") && !btnTexto.equals("/")) && !this.acumulado.isEmpty()) {
-            	
-                this.numero = btnTexto;
-                this.acumulado += this.numero;
-                tfContador.setText(this.acumulado);
-                
-            } else if (btnTexto.equals("+") || btnTexto.equals("-") || btnTexto.equals("*") || btnTexto.equals("/")) {
-            	
-            	añadirSigno(btnTexto);
-            }
-            
         } else {
-        	
-            if ((!btnTexto.equals("+") && !btnTexto.equals("-") && !btnTexto.equals("*") && !btnTexto.equals("/"))) {
             	
-                if (this.acumulado.equals("")) {
-                    tfContador.setText(btnTexto);
+        	if ((!btnTexto.equals("+") && !btnTexto.equals("-") && !btnTexto.equals("*") && !btnTexto.equals("/") && !btnTexto.equals("=") && !btnTexto.equals("CE"))) {
+                	
+                añadirDigitos(btnTexto);
+                    
+        	} else {
+                	
+        		switch (this.operador) {
+        			case "+":
+        				contador.sumarContador(Integer.parseInt(this.acumulado));
+        				tfContador.setText(Integer.toString(contador.getValor()));
+        				this.acumulado = "";
+        				this.operador = "";
+                        break;
+                            
+                        case "-":
+                        	contador.restarContador(Integer.parseInt(this.acumulado));
+                            tfContador.setText(Integer.toString(contador.getValor()));
+                            this.acumulado = "";
+                            this.operador = "";
+                            break;
+                            
+                        case "*":
+                            contador.multiplicarContador(Integer.parseInt(this.acumulado));
+                            tfContador.setText(Integer.toString(contador.getValor()));
+                            this.acumulado = "";
+                            this.operador = "";
+                            break;
+                            
+                        case "/":
+                            try {
+                                contador.dividirContador(Integer.parseInt(this.acumulado));
+                                tfContador.setText(Integer.toString(contador.getValor()));
+                                this.acumulado = "";
+                                this.operador = "";
+                            } catch (NumberFormatException e) {
+                                tfContador.setText("Error al dividir por 0");
+                                e.printStackTrace();
+                            }
+                            break;                          
+                            
+                        default:
+                			System.out.println("Opcion incorrecta");
+                    }  		                   
                 }
-                
-                this.numero = btnTexto;
-                this.acumulado += this.numero;
-                tfContador.setText(this.acumulado);
-                
-            } else {
-            	
-                switch (this.operador) {
-                    case "+":
-                        contador.sumarContador(Integer.parseInt(this.acumulado));
-                        break;
-                    case "-":
-                        contador.restarContador(Integer.parseInt(this.acumulado));
-                        break;
-                    case "*":
-                        contador.multiplicarContador(Integer.parseInt(this.acumulado));
-                        break;
-                    case "/":
-                        try {
-                            contador.dividirContador(Integer.parseInt(this.acumulado));
-                        } catch (NumberFormatException e) {
-                            tfContador.setText("Error al dividir por 0");
-                            e.printStackTrace();
-                        }
-                        break;
-                }
-                tfContador.setText(Integer.toString(contador.getValor()));
-
-                this.acumulado = "";
             }
-        }
-
-        if (btnTexto.equals("CE")) {
-            tfContador.clear();
-            this.operador = "";
-            this.acumulado = "";
-            
-        }
-
-        if (btnTexto.equals("=")) {
-        	
-            int resultado = contador.getValor();
+        
+        if(btnTexto.equals("=")) {
+        	int resultado = contador.getValor();
             tfContador.setText(Integer.toString(resultado));
+            
+            contador.clearAndIgual();
+            
             this.operador = "";
             this.acumulado = "";
+            this.numero = "";
+        }
+        if (btnTexto.equals("CE")) {
+        	
+            tfContador.clear();
+            
+            contador.clearAndIgual();
+            
+            this.operador= "";
+            this.acumulado = "";
+            this.numero = "";
         }
     }
 }
--- src/modelo/Contador.java
+++ src/modelo/Contador.java
@@ -9,46 +9,47 @@ public class Contador {
 		this.valor = 0;
 	}
 
+	
 	public int getValor() {
 		return valor;
 	}
 
+	
 	public void setValor(int valor) {
 		this.valor = valor;
 	}
 	
-	public void sumarContador(int segundoNumero) {
-		
+	
+	public void sumarContador(int segundoNumero) {		
 		int total = this.getValor() + segundoNumero;
-		
 		this.setValor(total);		
 	}
 	
+	
 	public void restarContador(int segundoNumero) {
-		
-		int total = this.getValor() - segundoNumero;
-		
+		int total = this.getValor() - segundoNumero;		
 		this.setValor(total);
 	}
+	
 	
 	public void multiplicarContador(int segundoNumero) {
-		
 		int total = this.getValor() * segundoNumero;
-		
 		this.setValor(total);
 	}
 	
+	
 	public void dividirContador(int segundoNumero) throws NumberFormatException {
-		
 		if( segundoNumero == 0) {
 			throw new NumberFormatException ("No se puede dividir por 0");
+			
 		} else {
-			
 			int total = this.getValor() / segundoNumero;
-			
 			this.setValor(total);
-		}
-		
-		
+		}		
+	}
+
+	
+	public void clearAndIgual(){
+		this.setValor(0);
 	}
 }
