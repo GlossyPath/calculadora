41c880870c65f0613c56c372c6410c76bbe211ff
GlossyPath <glossypath0808@gmail.com>
2024-07-24 23:51:59 +0200

    actualizado

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,31 +1,27 @@
 package application;
-	
+
 import javafx.application.Application;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
 
-
 public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			
-			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("/vista/CalculadoraFX.fxml"));
-			Scene scene = new Scene(root);
-			scene.getStylesheets().add(getClass().getResource("/vista/css/application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-			
-			primaryStage.setTitle("CalculadoraFx");
-			
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            AnchorPane root = (AnchorPane) FXMLLoader.load(getClass().getResource("/vista/CalculadoraFX.fxml"));
+            Scene scene = new Scene(root);
+            scene.getStylesheets().add(getClass().getResource("/vista/css/application.css").toExternalForm());
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("CalculadoraFx");
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
